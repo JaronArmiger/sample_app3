c878d04c6f4c3db042684a6602f1482cb7bc1af0
JaronArmiger <jaron.armiger@gmail.com>
2020-04-17 19:47:23 -0500

    finish password resets

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,9 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "account activation lmao"
   end
 
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: user.email, subject: "password reset lmao"
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,15 +1,26 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test "account activation" do
+  test "account_activation" do
     user = users(:alfonso)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
     assert_equal "account activation lmao", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["supinchemadre@ahuev.o"], mail.from
+    assert_equal ["supinchemadre@ahuev.o"],  mail.from
     assert_match user.name,                  mail.body.encoded
     assert_match user.activation_token,      mail.body.encoded
     assert_match CGI.escape(user.email),     mail.body.encoded
+  end
+
+  test "password_reset" do
+    user = users(:alfonso)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "password reset lmao", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["supinchemadre@ahuev.o"], mail.from
+    assert_match user.reset_token,          mail.body.encoded
+    assert_match CGI.escape(user.email),    mail.body.encoded
   end
 end
